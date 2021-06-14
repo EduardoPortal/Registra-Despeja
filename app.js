3fb2cc3908bb0a1a22d151281937f3eef02de804
Eduardo Portal <71727535+EduardoPortal@users.noreply.github.com>
2021-06-14 16:51:09 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 class Despesa {
-    constructor(ano, mes, dia , tipo , descricao, valor){
+    constructor(ano, mes, dia, tipo, descricao, valor) {
         this.ano = ano
         this.mes = mes
         this.dia = dia
@@ -8,9 +8,9 @@ class Despesa {
         this.valor = valor
     }
 
-    validarDados(){
-        for(let i in this){
-            if(this[i] == undefined || this[i]=='' || this[i] == null){
+    validarDados() {
+        for (let i in this) {
+            if (this[i] == undefined || this[i] == '' || this[i] == null) {
                 return false
             }
         }
@@ -18,65 +18,228 @@ class Despesa {
     }
 }
 
-class Bd{
+class Bd {
 
-    constructor(){
+    constructor() {
         let id = localStorage.getItem('id')
 
-        if(id === null){
+        if (id === null) {
             localStorage.setItem('id', 0)
         }
     }
 
-    getProximoId(){
+    getProximoId() {
         let proximoId = localStorage.getItem('id') //null
-       return parseInt(proximoId) + 1
-        }
+        return parseInt(proximoId) + 1
+    }
 
-    gravar(d){
-        
-       let id = this.getProximoId()
+    gravar(d) {
 
-       localStorage.setItem(id, JSON.stringify(d))
+        let id = this.getProximoId()
+
+        localStorage.setItem(id, JSON.stringify(d))
 
         localStorage.setItem('id', id)
+    }
+
+    recuperarTodosRegistros() {
+
+        //array de despesas
+        let despesas = Array()
+
+        let id = localStorage.getItem('id')
+
+        //recuperarr todas as depesas cadastradas
+        for (let i = 1; i <= id; i++) {
+
+            //recuperar a despesa
+            let despesa = JSON.parse(localStorage.getItem(i))
+
+            //existe a  possibilidade de haver indices que foram pulados/ null  pular os indice
+            if (despesa === null) {
+                continue
+            }
+
+            despesa.id = i
+            despesas.push(despesa)
+        }
+        return despesas
+    }
+    pesquisar(despesa) {
+        let despesasFiltradas = Array()
+        despesasFiltradas = this.recuperarTodosRegistros()
+
+        console.log(despesa)
+
+        console.log(despesasFiltradas)
+        //ano
+        if (despesa.ano != '') {
+            console.log('ANo filtrado')
+            despesasFiltradas = despesasFiltradas.filter(d => d.ano == despesa.ano)
+        }
+
+        //mes
+        if (despesa.mes != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.mes == despesa.mes)
+        }
+
+        //dia
+        if (despesa.dia != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.dia == despesa.dia)
+        }
+
+        //tipo
+        if (despesa.tipo != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.tipo == despesa.tipo)
+        }
+        //descricao
+        if (despesa.descricao != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.descricao == despesa.descricao)
+        }
+        //valor
+        if (despesa.valor != '') {
+            despesasFiltradas = despesasFiltradas.filter(d => d.valor == despesa.valor)
+        }
+
+        return despesasFiltradas
+    }
+
+    remover(id){
+        localStorage.removeItem(id)
     }
 }
 
 let bd = new Bd()
 
-function cadastrarDespesa(){
+function cadastrarDespesa() {
 
     let ano = document.getElementById('ano')
-    let mes =  document.getElementById('mes')
-    let dia =  document.getElementById('dia')
-    let tipo =  document.getElementById('tipo')
-    let descricao =  document.getElementById('descricao')
-    let valor =  document.getElementById('valor')
+    let mes = document.getElementById('mes')
+    let dia = document.getElementById('dia')
+    let tipo = document.getElementById('tipo')
+    let descricao = document.getElementById('descricao')
+    let valor = document.getElementById('valor')
 
 
-    let despesa = new Despesa(ano.value, mes.value ,dia.value, tipo.value, descricao.value, valor.value)
+    let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
 
-    if(despesa.validarDados()){
-    bd.gravar(despesa)
+    if (despesa.validarDados()) {
+        bd.gravar(despesa)
 
-    document.getElementById('modal_titulo').innerHTML = 'Registro com Sucesso'
-    document.getElementById('modal_titulo_div').className = 'modal-header text-success'
-    document.getElementById('modal_conteudo').innerHTML = 'Despeja cadastrada com legalidade'
-    document.getElementById('modal_botao').innerHTML=' voltar'
-    document.getElementById('modal_botao').className=' btn btn-success'
-    
-    //dialog de sucesso
-    $('#modalRegistraDespesa').modal('show')
-    }else{
+        document.getElementById('modal_titulo').innerHTML = 'Registro com Sucesso'
+        document.getElementById('modal_titulo_div').className = 'modal-header text-success'
+        document.getElementById('modal_conteudo').innerHTML = 'Despeja cadastrada com legalidade'
+        document.getElementById('modal_botao').innerHTML = ' voltar'
+        document.getElementById('modal_botao').className = ' btn btn-success'
+
+
+        ano.value = '',
+            mes.value = '',
+            dia.value = '',
+            tipo.value = '',
+            descricao.value = '',
+            valor.value = ''
+
+        //dialog de sucesso
+        $('#modalRegistraDespesa').modal('show')
+    } else {
 
         document.getElementById('modal_titulo').innerHTML = 'Erro na inclusão'
         document.getElementById('modal_titulo_div').className = 'modal-header text-danger'
         document.getElementById('modal_conteudo').innerHTML = 'Despeja não cadastrada'
-        document.getElementById('modal_botao').innerHTML=' voltar e corrigir'
-        document.getElementById('modal_botao').className=' btn btn-danger'
+        document.getElementById('modal_botao').innerHTML = ' voltar e corrigir'
+        document.getElementById('modal_botao').className = ' btn btn-danger'
 
-    //dialog fracasso
-        $('#modalRegistraDespesa').modal('show') 
+        //dialog fracasso
+        $('#modalRegistraDespesa').modal('show')
     }
+}
+
+
+function carregaListaDespesas(despesas = Array(), filtro = false) {
+
+    if (despesas.length == 0 && filtro == false) {
+        despesas = bd.recuperarTodosRegistros()
+    }
+
+    let listaDespesas = document.getElementById('listaDespesas')
+    listaDespesas.innerHTML = ''
+
+    /*<tr>
+        <td>15/03/2018</td>
+        <td>Alimentação</td>
+        <td>Compra dos mes</td>
+        <td>24,44</td>
+    </tr>*/
+
+    //percorrer o Array despesa testando cada despesa
+    despesas.forEach(function (d) {
+
+        console.log(d)
+
+        //CRIANDO A LINHA(TR)
+        let linha = listaDespesas.insertRow()
+
+        //criar ar coluna(td)
+
+        linha.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`
+
+        //ajustar o tipo
+        switch (d.tipo) {
+            case '1': d.tipo = 'Alimentação'
+                break;
+            case '2': d.tipo = 'Educação'
+                break;
+
+            case '3': d.tipo = 'Lazer'
+                break;
+
+            case '4': d.tipo = 'Saúde'
+                break;
+
+            case '5': d.tipo = 'Transporte'
+                break;
+
+        }
+
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+
+        //criar o botaol de exclusão
+
+        let btn = document.createElement("button")
+        btn.className = 'btn btn-info'
+        btn.innerHTML = '<i class="fas fa-times"></i>'
+        btn.id = `id_despesa_${d.id}`
+        btn.onclick = function(){
+            //remover a despesa
+            
+            let id = this.id.replace('id_despesa_', '')
+
+           bd.remover(id)
+
+           window.location.reload()
+        }
+        linha.insertCell(4).append(btn)
+
+        console.log(d)
+    })
+
+}
+
+function pesquisarDespesa() {
+    let ano = document.getElementById('ano').value
+    let mes = document.getElementById('mes').value
+    let dia = document.getElementById('dia').value
+    let tipo = document.getElementById('tipo').value
+    let descricao = document.getElementById('descricao').value
+    let valor = document.getElementById('valor').value
+
+    let despesa = new Despesa(ano, mes, dia, tipo, descricao, valor)
+
+    let despesas = bd.pesquisar(despesa)
+
+    carregaListaDespesas(despesas, true)
+
 }
